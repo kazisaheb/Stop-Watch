748d9bc0257c1de9028056ed35eadf1f5f502365
kazisaheb <kzsaheb@gmail.com>
2022-03-03 15:05:19 +0600

    Code shorthen

--- js.js
+++ js.js
@@ -7,20 +7,20 @@ stopBtn = document.getElementById('stopBtn')
 stopWatch = () => {
   startBtn.style.display = 'none'
   stopBtn.style.display = 'block'
-  minutes.innerText = 00
-  seconds.innerText = 00
-  milisec.innerText = 00
+  minutes.innerText = 0
+  seconds.innerText = 0
+  milisec.innerText = 0
 
   count = 0
   watch = setInterval(() => {
     milisec.innerText = count++
     if (count > 99) {
       count = 0
-      seconds.innerText = +seconds.innerText + 1
+      seconds.innerText++
     }
     if (seconds.innerText > 59) {
       seconds.innerText = 0
-      minutes.innerText = +minutes.innerText + 1
+      minutes.innerText++
     }
   }
     , 10
