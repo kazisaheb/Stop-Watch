a8f08a13b5c47222709c6673d6e47548c2c4fe0d
kazisaheb <kzsaheb@gmail.com>
2022-03-03 13:10:50 +0600

    All function done

--- js.js
+++ js.js
@@ -1,21 +1,31 @@
 minutes = document.getElementById('minutes')
 seconds = document.getElementById('seconds')
 milisec = document.getElementById('milisec')
+start = document.getElementById('start')
 
-start = () => {
-  document.getElementById('start').setAttribute("disabled", true)
+stopWatch = () => {
+  start.setAttribute("disabled", true)
+  minutes.innerText = 00
+  seconds.innerText = 00
+  milisec.innerText = 00
 
   count = 0
   watch = setInterval(() => {
     milisec.innerText = count++
-    seconds.innerText = Math.floor(count / 10)
-    minutes.innerText = Math.floor(seconds.innerText / 10)
+    if (count > 99) {
+      count = 0
+      seconds.innerText = +seconds.innerText + 1
+    }
+    if (seconds.innerText > 59) {
+      seconds.innerText = 0
+      minutes.innerText = +minutes.innerText + 1
+    }
   }
-    , 100
+    , 10
   )
 }
 stop = () => {
   clearInterval(watch)
-  document.getElementById('start').removeAttribute("disabled")
+  start.removeAttribute("disabled")
 }
 
